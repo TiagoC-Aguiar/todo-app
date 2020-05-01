8809d33b2d04504dccca9eff12881f079e459fd9
Tiago Aguiar <tyago_tka48@hotmail.com>
2020-05-01 20:16:52 -0300

    get and post on lists

---
+++ backend/src/database/migrations/20200501192635_create_lists.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('lists', (table) => {
+    table.increments('id').primary()
+    table.string('title').notNullable()
+
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable('users')
+
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('lists')
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 const SessionController = require('./controllers/SessionController')
 
+const connection = require('./database/connection')
+
 routes.post('/sessions', SessionController.create)
 
 routes.get('/users', UserController.index)
@@ -10,5 +12,29 @@ routes.post('/users', UserController.create)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
 
+routes.get('/lists', async (request, response) => {
+  const id = request.headers.authorization
+  const lists = await connection('lists')
+    .where('user_id', id)
+    .select('*')     
+  if(!lists) {
+    return response.json({ message: 'Não encontrado' })
+  }
+  return response.json(lists)  
+})
+
+
+routes.post('/lists', async (request, response) => {
+  const { title } = request.body
+  const user_id = request.headers.authorization
+
+  console.log(`***** aqui ===> ${title} e ${user_id}`)
+
+  await connection('lists').insert({
+    title, user_id
+  })
+
+  return response.json({ message: 'Lista cadastrada com sucesso' })
+})
 
 module.exports = routes
